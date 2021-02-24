69eb4909462b35b9590568b022f3b6a4bfd03323
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-02-24 09:27:31 -0800

    corrected landing page file path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/about" component={About} />
+          <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />

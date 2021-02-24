5cd31cefedcdc5435ce2553c73cb1e036ae1fb0b
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-02-24 10:01:08 -0800

    fixed weather dashboard url

--- src/components/Jumbotron/Project5/index.js
+++ src/components/Jumbotron/Project5/index.js
@@ -5,7 +5,7 @@ function Project5() {
     return (
         <div class="col-lg-4 col-sm-6">
             <h3 class="img-caption">Weather Dashboard</h3>
-            <a href="hhttps://vantango.github.io/weather-dashboard/" target="_blank">
+            <a href="https://vantango.github.io/weather-dashboard/" target="_blank">
                 <img src="https://github.com/vantango/weather-dashboard/raw/main/images/weather-dashboard.png?raw=true"
                     alt="weather-dashboard-image" class="img-thumbnail" />
             </a>

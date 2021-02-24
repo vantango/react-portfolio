cb34617ceb3a76989793cb04985bc7dbe6797eb4
Aleks Kimball-Moreno <alekskimball@gmail.com>
2021-02-24 09:51:24 -0800

    fixed mislabeled project title

--- src/components/Jumbotron/Project5/index.js
+++ src/components/Jumbotron/Project5/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function Project5() {
     return (
         <div class="col-lg-4 col-sm-6">
-            <h3 class="img-caption">Burger Devourer</h3>
+            <h3 class="img-caption">Weather Dashboard</h3>
             <a href="hhttps://vantango.github.io/weather-dashboard/" target="_blank">
                 <img src="https://github.com/vantango/weather-dashboard/raw/main/images/weather-dashboard.png?raw=true"
                     alt="weather-dashboard-image" class="img-thumbnail" />
